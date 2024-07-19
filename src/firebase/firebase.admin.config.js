80b689f3d799a013989b37e1189e7792e8277b57
dev웅 <dev.lsw91@gmail.com>
2024-07-19 11:22:45 +0900

    config

--- src/firebase/firebase.admin.config.js
+++ src/firebase/firebase.admin.config.js
@@ -1,8 +1,20 @@
 import {getFirestore} from "firebase-admin/firestore";
 const { getStorage } = require('firebase-admin/storage');
-const serviceAccount = require("./my-sns-healthtar-firebase-adminsdk-rzzkd-dbd3258b98.json");
-
 const {initializeApp, getApp, getApps, cert} = require('firebase-admin/app');
+
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 let app;
 if (!getApps().length) {
